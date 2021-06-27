5a01449a83c1bf2420b9d0ec13cdc5874028625c
kieran lynch <k.j.lynch@icloud.com>
2021-06-27 11:59:42 +0100

    numbers concatenate again and an operator btn replaces the previous numbers entered

--- calculatorApp.js
+++ calculatorApp.js
@@ -1,35 +1,44 @@
-//declared a var equal to the div containing all the buttons 
+//declared a global var equal to the div containing all the buttons 
 const wrapperBtns = document.querySelector(".wrapper__buttons");
 
-// var which targets the div for the display
+// declared a global var which targets the div for the display
 const display = document.querySelector(".wrapper__display");
 
 // added an eventlistener to the btnClicked variable, 
 wrapperBtns.addEventListener("click", event => {
   //used event.target to get the info about what value is being clicked
 const btnClicked = event.target;
+//set the value of clickedValue to be equal to the button being clicked
 const clickedValue = btnClicked.textContent;
 const displayedValue = display.textContent;
+//var to replace btnClicked.dataset.type
+const { type } = btnClicked.dataset
+
+const { previousBtnType } = wrapperBtns.dataset
 
 // inputs the displayed value into the display div
 display.textContent = clickedValue;
 
 // checks if the button is a number
-if (btnClicked.classlist.contains(".number")) {
+if (type == "number") {
   // if displayed value is zero, replace it with the button value, if it's a number
   if (displayedValue == 0) {
   display.textContent = clickedValue;
+  } 
+  // else if to refresh the display if the previous btn was an operator
+  else if (previousBtnType == "operator") 
+  {
+    display.textContent = clickedValue
   } else {
     //concatenate the values
     display.textContent = displayedValue + clickedValue;
   }
 }
 // checks if the button is an operator
-// checks if the button being clicked has a dataset of operator in the html
-
-if (btnClicked.dataset.type(".operator")) {
+// checks if the button being clicked has a custom atttribute "dataset" in the html
+if (type = "operator") {
   console.log(btnClicked);
 
-  document.dataset.previousKeyType = "operator"
+  document.dataset.previousBtnType = "operator"
 }
 })
--- dist/calculatorApp.dev.js
+++ dist/calculatorApp.dev.js
@@ -1,32 +1,41 @@
 "use strict";
 
-//declared a var equal to the div containing all the buttons 
-var wrapperBtns = document.querySelector(".wrapper__buttons"); // var which targets the div for the display
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
+//declared a global var equal to the div containing all the buttons 
+var wrapperBtns = document.querySelector(".wrapper__buttons"); // declared a global var which targets the div for the display
 
 var display = document.querySelector(".wrapper__display"); // added an eventlistener to the btnClicked variable, 
 
 wrapperBtns.addEventListener("click", function (event) {
   //used event.target to get the info about what value is being clicked
-  var btnClicked = event.target;
+  var btnClicked = event.target; //set the value of clickedValue to be equal to the button being clicked
+
   var clickedValue = btnClicked.textContent;
-  var displayedValue = display.textContent; // inputs the displayed value into the display div
+  var displayedValue = display.textContent; //var to replace btnClicked.dataset.type
+
+  var type = btnClicked.dataset.type;
+  var previousBtnType = wrapperBtns.dataset.previousBtnType; // inputs the displayed value into the display div
 
   display.textContent = clickedValue; // checks if the button is a number
 
-  if (btnClicked.classlist.contains(".number")) {
+  if (type == "number") {
     // if displayed value is zero, replace it with the button value, if it's a number
     if (displayedValue == 0) {
       display.textContent = clickedValue;
-    } else {
-      //concatenate the values
-      display.textContent = displayedValue + clickedValue;
-    }
+    } // else if to refresh the display if the previous btn was an operator
+    else if (previousBtnType == "operator") {
+        display.textContent = clickedValue;
+      } else {
+        //concatenate the values
+        display.textContent = displayedValue + clickedValue;
+      }
   } // checks if the button is an operator
-  // checks if the button being clicked has a dataset of operator in the html
+  // checks if the button being clicked has a custom atttribute "dataset" in the html
 
 
-  if (btnClicked.dataset.type(".operator")) {
+  if (type = (_readOnlyError("type"), "operator")) {
     console.log(btnClicked);
-    document.dataset.previousKeyType = "operator";
+    document.dataset.previousBtnType = "operator";
   }
 });
